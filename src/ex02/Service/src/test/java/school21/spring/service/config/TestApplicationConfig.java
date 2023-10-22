311f48a8620d253ea01169d9f68082b4466a58e4
comedial <comedial@student.21-school.ru>
2023-10-22 22:35:27 +0300

    tests

--- src/ex02/Service/src/test/java/school21/spring/service/config/TestApplicationConfig.java
+++ src/ex02/Service/src/test/java/school21/spring/service/config/TestApplicationConfig.java
@@ -1,4 +1,45 @@
 package school21.spring.service.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import school21.spring.service.repositories.UsersRepository;
+import school21.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
+import school21.spring.service.services.UsersServiceImpl;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@ComponentScan(basePackages = "school21.spring.service")
 public class TestApplicationConfig {
+
+    @Primary
+    @Bean
+    @Qualifier("driverManagerBean")
+    public DriverManagerDataSource driverManagerDataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl("jdbc:h2:mem:testdb");
+        driverManagerDataSource.setUsername("sa");
+        driverManagerDataSource.setPassword("");
+        driverManagerDataSource.setDriverClassName("org.h2.Driver");
+        return driverManagerDataSource;
+    }
+    @Primary
+    @Bean
+    @Qualifier("usersRepositoryTestBean")
+    public UsersRepositoryJdbcTemplateImpl usersRepositoryJdbcTemplate (@Qualifier("driverManagerBean") DataSource dataSource) {
+        return new UsersRepositoryJdbcTemplateImpl(dataSource);
+    }
+
+    @Primary
+    @Bean
+    @Qualifier("usersServiceTestBean")
+    public UsersServiceImpl usersService (@Qualifier("UsersRepositoryJdbcTemplateImpl") UsersRepository usersRepository) {
+       return new UsersServiceImpl(usersRepository);
+    }
+
 }
--- src/ex02/Service/src/test/java/school21/spring/service/services/UsersServiceImplTest.java
+++ src/ex02/Service/src/test/java/school21/spring/service/services/UsersServiceImplTest.java
@@ -1,4 +1,35 @@
 package school21.spring.service.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import school21.spring.service.config.TestApplicationConfig;
+import school21.spring.service.repositories.UsersRepository;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {TestApplicationConfig.class})
 public class UsersServiceImplTest {
+    @Autowired
+    private UsersService usersService;
+
+    @Mock
+    private UsersRepository usersRepository;
+
+    @Test
+    @Sql("/table.sql")
+    void signUpTest() {
+        ((UsersServiceImpl)usersService).setUsersRepository(usersRepository);
+
+        String password = usersService.signUp("test@mail.ru");
+        Mockito.verify(usersRepository, Mockito.times(1)).save(Mockito.any());
+
+        Assertions.assertNotNull(password);
+    }
 }
